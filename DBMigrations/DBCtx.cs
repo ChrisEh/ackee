a7edb192e649f3d0ba6f4b6d7b4a66c5ed30b0e0
Grünewald,Chris C.A <chris.grunewald@cytosmart.com>
2019-11-06 14:20:57 +0100

    Created DBMigrationProject and user table migration

---
+++ DBMigrations/DBCtx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMigrations
+{
+    public class DBCtx : DbContext
+    {
+        public DBCtx() : base("name=DBCtx")
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+    }
+}
---
+++ DBMigrations/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DBMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DBMigrations.DBCtx>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DBMigrations.DBCtx context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
