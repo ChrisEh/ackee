27f7eab6855360fc3f31cb1c0d50fa8a817d57e8
Grünewald,Chris C.A <chris.grunewald@cytosmart.com>
2019-11-12 13:41:51 +0100

    Added new migration for fixing Application user error.

--- Ackee/Data/Migrations/20191112124031_ExtendedApplicationUserAndProjectsTable.Designer.cs
+++ Ackee/Data/Migrations/20191112124031_ExtendedApplicationUserAndProjectsTable.Designer.cs
@@ -4,14 +4,16 @@ using Ackee.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ackee.Data.Migrations
 {
     [DbContext(typeof(AckeeCtx))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191112124031_ExtendedApplicationUserAndProjectsTable")]
+    partial class ExtendedApplicationUserAndProjectsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
