8b774fcc2cf4e48b2a80a26590978cf914bb43d8
Grünewald,Chris C.A <chris.grunewald@cytosmart.com>
2019-11-12 11:55:13 +0100

    Fixed namespace for data.model, added user table

--- Ackee/Data/AckeeCtx.cs
+++ Ackee/Data/AckeeCtx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using AckeeDb.Data.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,80 +16,5 @@ namespace Ackee.Data
             : base(options)
         {
         }
-        
-        // TODO: Make fields for context work
-        //public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
-        //public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
-        //public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
-        //public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
-        //public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
-        //public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
-        //public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //    }
-        //}
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
-
-        //    modelBuilder.Entity<AspNetRoleClaims>(entity =>
-        //    {
-        //        entity.HasIndex(e => e.RoleId);
-        //    });
-
-        //    modelBuilder.Entity<AspNetRoles>(entity =>
-        //    {
-        //        entity.HasIndex(e => e.NormalizedName)
-        //            .HasName("RoleNameIndex")
-        //            .IsUnique()
-        //            .HasFilter("([NormalizedName] IS NOT NULL)");
-
-        //        entity.Property(e => e.Id).ValueGeneratedNever();
-        //    });
-
-        //    modelBuilder.Entity<AspNetUserClaims>(entity =>
-        //    {
-        //        entity.HasIndex(e => e.UserId);
-        //    });
-
-        //    modelBuilder.Entity<AspNetUserLogins>(entity =>
-        //    {
-        //        entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
-
-        //        entity.HasIndex(e => e.UserId);
-        //    });
-
-        //    modelBuilder.Entity<AspNetUserRoles>(entity =>
-        //    {
-        //        entity.HasKey(e => new { e.UserId, e.RoleId });
-
-        //        entity.HasIndex(e => e.RoleId);
-        //    });
-
-        //    modelBuilder.Entity<AspNetUserTokens>(entity =>
-        //    {
-        //        entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
-        //    });
-
-        //    modelBuilder.Entity<AspNetUsers>(entity =>
-        //    {
-        //        entity.HasIndex(e => e.NormalizedEmail)
-        //            .HasName("EmailIndex");
-
-        //        entity.HasIndex(e => e.NormalizedUserName)
-        //            .HasName("UserNameIndex")
-        //            .IsUnique()
-        //            .HasFilter("([NormalizedUserName] IS NOT NULL)");
-
-        //        entity.Property(e => e.Id).ValueGeneratedNever();
-        //    });
-        //}
-
-        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
---
+++ Ackee/Data/Model/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ackee.Data.Model
+{
+    public partial class Users : AspNetUsers
+    {
+        public int PCN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
