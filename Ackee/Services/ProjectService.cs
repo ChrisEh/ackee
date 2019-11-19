0a866801a1875a1e1ccfe9ee8c87f29a088f9334
Denis Gorianin <goryaninden@gmail.com>
2019-11-19 15:40:48 +0100

    Initial commit

---
+++ Ackee/Services/ProjectService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ackee.Data;
+using Ackee.Data.Model;
+
+namespace Ackee.Services
+{
+    public class ProjectService
+    {
+        private AckeeCtx _context;
+        public ProjectService(AckeeCtx context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Projects>> GetProjects()
+        {
+            List<Projects> listOfProjects = new List<Projects>();
+
+            return listOfProjects;
+        }
+    }
+}
