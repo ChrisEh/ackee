e0abe71c97dda58e52ceaf6677f9a3c641158bba
Alex Lievense <alexlievense@gmail.com>
2019-12-24 11:52:29 +0100

    Fixed left menu component to have shadow on the right side, also displays active page correctly.

---
+++ Ackee/Shared/Services/StringManipulationService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ackee.Shared.Services
+{    
+    public class StringManipulationService
+    {
+        // 
+        public string GetStringAbbreviation(string input)
+        {
+            string abbreviation = "";
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return abbreviation;
+            }
+
+            List<string> separateWords = input.Split(' ').ToList();
+
+            if (separateWords.Count == 1)
+            {
+                abbreviation = separateWords[0][0].ToString();
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    abbreviation += separateWords[i][0].ToString();
+                }
+            }
+
+            return abbreviation.ToUpper();
+        }
+
+        public string CapitalizeFirstLetterOfString(string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+
+        public string ToTitleCase(string input)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+        }
+    }
+}
--- Ackee/Startup.cs
+++ Ackee/Startup.cs
@@ -54,7 +54,7 @@ namespace Ackee
                 RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
             services.AddSingleton<WeatherForecastService>();
             services.AddScoped<ProjectController>();
-            services.AddScoped<ProjectService>();
+            services.AddScoped<StringManipulationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
